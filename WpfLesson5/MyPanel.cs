396241999d4232fff4de37c1fe159c83f57a9553
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:12:02 +0300

    Add project files.

---
+++ WpfLesson5/MyPanel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLesson5
+{
+    public class MyPanel : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size size = new Size();
+            foreach (UIElement Child in InternalChildren)
+            {
+                Child.Measure(availableSize);
+                size = Child.DesiredSize;
+            }
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            double x = 0;
+            double y = 0;
+            //foreach (UIElement Child in InternalChildren)
+            //{
+            //    Child.Arrange(new Rect(new Point(x, y), Child.DesiredSize));
+            //}
+            for (int i=0;i<InternalChildren.Count;i++)
+            {
+                InternalChildren[i].Arrange(new Rect(new Point(x, y), InternalChildren[i].DesiredSize));
+                x += InternalChildren[i].DesiredSize.Width;
+                y += InternalChildren[i].DesiredSize.Height;
+            }
+            return finalSize;
+        }
+
+        public MyPanel() : base()
+        {
+
+        }
+    }
+}
---
+++ WpfLesson5/Page2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLesson5
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            //Page1 page1 = new Page1();
+            NavigationService.Navigate(new Uri("Page1.xaml"), UriKind.Relative);
+        }
+    }
+}
